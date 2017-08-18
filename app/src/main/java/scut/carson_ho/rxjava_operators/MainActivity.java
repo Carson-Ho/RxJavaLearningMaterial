a04be9adb5a5c2cc8e86aa580a4d5c28dbe9ec2d
Carson.Ho <carson.ho@foxmail.com>
2017-08-18 16:37:22 +0800

    Initial commit

---
+++ app/src/main/java/scut/carson_ho/rxjava_operators/MainActivity.java
@@ -0,0 +1,65 @@
+package scut.carson_ho.rxjava_operators;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "RxJava";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+// 1. 通过creat（）创建被观察者对象
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            // 2. 在复写的subscribe（）里定义需要发送的事件
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+
+                emitter.onComplete();
+            }  // 至此，一个被观察者对象（Observable）就创建完毕
+        }).subscribe(new Observer<Integer>() {
+            // 以下步骤仅为展示一个完整demo，可以忽略
+            // 3. 通过通过订阅（subscribe）连接观察者和被观察者
+            // 4. 创建观察者 & 定义响应事件的行为
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用subscribe连接");
+            }
+            // 默认最先调用复写的 onSubscribe（）
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "接收到了事件"+ value  );
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+
+        });
+    }
+
+    }
+
+
