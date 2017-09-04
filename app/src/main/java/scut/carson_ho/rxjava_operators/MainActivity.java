5fc0305303359268e161fdb3c88cfaeb97ef3f97
Carson.Ho <carson.ho@foxmail.com>
2017-09-04 16:01:02 +0800

    add change

--- app/src/main/java/scut/carson_ho/rxjava_operators/MainActivity.java
+++ app/src/main/java/scut/carson_ho/rxjava_operators/MainActivity.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.List;
+
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,52 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                e.onNext(1);
-                e.onNext(2);
-                e.onError(new Exception("发生错误了"));
-                e.onNext(3);
-            }
-        })
-                .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
-                    @Override
-                    public ObservableSource<?> apply(@NonNull Observable<Throwable> throwableObservable) throws Exception {
-
-                        // 返回新的被观察者 Observable
-                        // 此处有两种情况：
-                            // 1. 原始的Observable不重新发送事件：新的被观察者 Observable发送的事件 = Error事件
-                            // 2. 原始的Observable重新发送事件：新的被观察者 Observable发送的事件 = 数据事件
-                        return throwableObservable.flatMap(new Function<Throwable, ObservableSource<?>>() {
-                            @Override
-                            public ObservableSource<?> apply(@NonNull Throwable throwable) throws Exception {
-
-                                // 1. 若返回的Observable发送的事件 = Error事件，则原始的Observable不重新发送事件
-                                // 该异常错误信息可在观察者中的onError（）中获得
-                                 // return Observable.error(new Throwable("retryWhen终止啦"));
-
-                                // 2. 若返回的Observable发送的事件 = 数据事件，则原始的Observable重新发送事件（若持续遇到错误，则持续重试）
-                                  return Observable.just(1);
-                            }
-                        });
-
-                    }
-                })
-                .subscribe(new Observer<Integer>() {
+        // 被观察者 需要发送5个数字
+        Observable.just(1, 2, 3, 4, 5)
+                .buffer(3, 1) // 设置缓存区大小 & 步长
+                                    // 缓存区大小 = 每次从被观察者中获取的事件数量
+                                    // 步长 = 每次获取新事件的数量
+                .subscribe(new Observer<List<Integer>>() {
                     @Override
                     public void onSubscribe(Disposable d) {
 
                     }
                     @Override
-                    public void onNext(Integer value) {
-                        Log.d(TAG, "接收到了事件"+ value  );
+                    public void onNext(List<Integer> stringList) {
+                        //
+                        Log.d(TAG, " 缓存区里的事件数量 = " +  stringList.size());
+                        for (Integer value : stringList) {
+                            Log.d(TAG, " 事件 = " + value);
+                        }
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.d(TAG, "对Error事件作出响应" + e.toString());
-                        // 获取异常错误信息
+                        Log.d(TAG, "对Error事件作出响应" );
                     }
 
                     @Override
@@ -76,6 +49,62 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(TAG, "对Complete事件作出响应");
                     }
                 });
+
+
+
+//        Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                e.onNext(1);
+//                e.onNext(2);
+//                e.onError(new Exception("发生错误了"));
+//                e.onNext(3);
+//            }
+//        })
+//                .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
+//                    @Override
+//                    public ObservableSource<?> apply(@NonNull Observable<Throwable> throwableObservable) throws Exception {
+//
+//                        // 返回新的被观察者 Observable
+//                        // 此处有两种情况：
+//                            // 1. 原始的Observable不重新发送事件：新的被观察者 Observable发送的事件 = Error事件
+//                            // 2. 原始的Observable重新发送事件：新的被观察者 Observable发送的事件 = 数据事件
+//                        return throwableObservable.flatMap(new Function<Throwable, ObservableSource<?>>() {
+//                            @Override
+//                            public ObservableSource<?> apply(@NonNull Throwable throwable) throws Exception {
+//
+//                                // 1. 若返回的Observable发送的事件 = Error事件，则原始的Observable不重新发送事件
+//                                // 该异常错误信息可在观察者中的onError（）中获得
+//                                 // return Observable.error(new Throwable("retryWhen终止啦"));
+//
+//                                // 2. 若返回的Observable发送的事件 = 数据事件，则原始的Observable重新发送事件（若持续遇到错误，则持续重试）
+//                                  return Observable.just(1);
+//                            }
+//                        });
+//
+//                    }
+//                })
+//                .subscribe(new Observer<Integer>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//
+//                    }
+//                    @Override
+//                    public void onNext(Integer value) {
+//                        Log.d(TAG, "接收到了事件"+ value  );
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.d(TAG, "对Error事件作出响应" + e.toString());
+//                        // 获取异常错误信息
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.d(TAG, "对Complete事件作出响应");
+//                    }
+//                });
 
 
 
